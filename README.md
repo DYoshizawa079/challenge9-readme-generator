8ac3a8b75c9c6b91a33fc963abe430c2f98f012a
Skapa804 <daniel@Daniels-MacBook-Pro-3.local>
2021-12-10 17:49:11 -0500

    Added most features except badges...albeit without testing

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-#appname
-##Description
-undefined
+# Myapp
+## Description
+Mydesc
+
+Deplyoment URL: [www.google.com](www.google.com)
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Credits/Contributing](#credits)
+* [Testing](#testing)
+* [Questions](#questions)
+## Installation
+Myinstall
+## Usage
+Myuse
+!([The app in use]https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Vought_F4U_Corsair_%28USMC%29.jpg/300px-Vought_F4U_Corsair_%28USMC%29.jpg)
+## License
+ISC,Mozilla Public License 2.0
+## Credits
+Daniel
+## Testing
+Mytest
+## Questions
+If you have any further questions, please contact the author at:
+- Email: [daniel@daniel.com](daniel@daniel.com)
+- Github URL: [https://github.com/DYoshizawa079](https://github.com/DYoshizawa079)
--- index.js
+++ index.js
@@ -16,6 +16,11 @@ inquirer.prompt([
     },
     {
         type: "input",
+        name: "Deployment",
+        message: "Give the URL of the deployed application."
+    },
+    {
+        type: "input",
         name: "Installation",
         message: "Give a description of how your project/application will be installed."
     },
@@ -26,8 +31,8 @@ inquirer.prompt([
     },
     {
         type: "input",
-        name: "Credits",
-        message: "Give names of people or organizations who were involved in the creation of this project/application."
+        name: "Usage_screenshot",
+        message: "Give a URL that shows a screenshot of the project."
     },
     {
         type: "checkbox",
@@ -37,13 +42,52 @@ inquirer.prompt([
     },
     {
         type: "input",
+        name: "Credits",
+        message: "Give names of people or organizations who were involved in the creation of this project/application."
+    },
+    {
+        type: "input",
         name: "Testing",
         message: "How would someone test your project/application."
     },
+    {
+        type: "input",
+        name: "Github_name",
+        message: "What's the name of the Github user that should be contacted for further information?"
+    },
+    {
+        type: "input",
+        name: "Email",
+        message: "What's the email address that should be contacted for further information?"
+    }
 ])
-.then(response => fs.writeFile("README.md", `#${response.Title}
-##Description
-${response.description}
+.then(response => fs.writeFile("README.md", `# ${response.Title}
+## Description
+${response.Description}
+
+Deplyoment URL: [${response.Deployment}](${response.Deployment})
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Credits/Contributing](#credits)
+* [Testing](#testing)
+* [Questions](#questions)
+## Installation
+${response.Installation}
+## Usage
+${response.Usage}
+!([The app in use]${response.Usage_screenshot})
+## License
+${response.License}
+## Credits
+${response.Credits}
+## Testing
+${response.Testing}
+## Questions
+If you have any further questions, please contact the author at:
+- Email: [${response.Email}](${response.Email})
+- Github URL: [https://github.com/${response.Github_name}](https://github.com/${response.Github_name})
 `, error => {
     if (error) throw error
 }))
